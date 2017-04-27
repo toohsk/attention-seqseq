cc8396eb5db491e23ba5dd360bc8c8cb2b5dd01f
Hara Atsushi <toohsk@gmail.com>
2017-04-27 11:49:21 +0900

    Add attention model

--- att_s2s_chainer.py
+++ att_s2s_chainer.py
@@ -184,6 +184,179 @@ class Seq2Seq(Chain):
 
 # In[ ]:
 
+class Att_LSTM_Decoder(Chain):
+    def __init__(self, vocab_size, embed_size, hidden_size):
+        super(Att_LSTM_Decoder, self).__init__(
+            ye = links.EmbedID(vocab_size, embed_size, ignore_label=-1),
+            eh = links.Linear(embed_size, 4 * hidden_size),
+            hh = links.Linear(hidden_size, 4 * hidden_size),
+            fh = links.Linear(hidden_size, 4 * hidden_size),
+            bh = links.Linear(hidden_size, 4 * hidden_size),
+            he = links.Linear(hidden_size, embed_size),
+            ey = links.Linear(embed_size, vocab_size)
+        )
+        
+    def __call__(self, y, c, f, b):
+        e = functions.tanh(self.ye(y))
+        c, h = functions.lstm(c, self.eh(e) + self.hh(h) + self.fh(f) + self.bh(b))
+        t = self.ey(functions.tanh(self.he(h)))
+        return t, c, h
+
+
+# In[ ]:
+
+class Attention(Chain):
+    def __init__(self, hidden_size, flag_gpu):
+        """
+        Attentionのインスタンス化
+        :param hidden_size: 隠れ層のサイズ
+        :param flag_gpu: GPUを使うかどうか
+        """
+        super(Attention, self).__init__(
+            # 順向きのEncoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
+            fh=links.Linear(hidden_size, hidden_size),
+            # 逆向きのEncoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
+            bh=links.Linear(hidden_size, hidden_size),
+            # Decoderの中間ベクトルを隠れ層サイズのベクトルに変換する線形結合層
+            hh=links.Linear(hidden_size, hidden_size),
+            # 隠れ層サイズのベクトルをスカラーに変換するための線形結合層
+            hw=links.Linear(hidden_size, 1),
+        )
+        # 隠れ層のサイズを記憶
+        self.hidden_size = hidden_size
+        # GPUを使う場合はcupyを使わないときはnumpyを使う
+        if flag_gpu:
+            self.ARR = cuda.cupy
+        else:
+            self.ARR = np
+
+    def __call__(self, fs, bs, h):
+        """
+        Attentionの計算
+        :param fs: 順向きのEncoderの中間ベクトルが記録されたリスト
+        :param bs: 逆向きのEncoderの中間ベクトルが記録されたリスト
+        :param h: Decoderで出力された中間ベクトル
+        :return: 順向きのEncoderの中間ベクトルの加重平均と逆向きのEncoderの中間ベクトルの加重平均
+        """
+        # ミニバッチのサイズを記憶
+        batch_size = h.data.shape[0]
+        # ウェイトを記録するためのリストの初期化
+        ws = []
+        # ウェイトの合計値を計算するための値を初期化
+        sum_w = Variable(self.ARR.zeros((batch_size, 1), dtype='float32'))
+        # Encoderの中間ベクトルとDecoderの中間ベクトルを使ってウェイトの計算
+        for f, b in zip(fs, bs):
+            # 順向きEncoderの中間ベクトル、逆向きEncoderの中間ベクトル、Decoderの中間ベクトルを使ってウェイトの計算
+            w = functions.tanh(self.fh(f)+self.bh(b)+self.hh(h))
+            # softmax関数を使って正規化する
+            w = functions.exp(self.hw(w))
+            # 計算したウェイトを記録
+            ws.append(w)
+            sum_w += w
+        # 出力する加重平均ベクトルの初期化
+        att_f = Variable(self.ARR.zeros((batch_size, self.hidden_size), dtype='float32'))
+        att_b = Variable(self.ARR.zeros((batch_size, self.hidden_size), dtype='float32'))
+        for f, b, w in zip(fs, bs, ws):
+            # ウェイトの和が1になるように正規化
+            w /= sum_w
+            # ウェイト * Encoderの中間ベクトルを出力するベクトルに足していく
+            att_f += functions.reshape(functions.batch_matmul(f, w), (batch_size, self.hidden_size))
+            att_b += functions.reshape(functions.batch_matmul(f, w), (batch_size, self.hidden_size))
+        return att_f, att_b
+
+
+# In[ ]:
+
+class Att_Seq2Seq(Chain):
+    def __init__(self, vocab_size, embed_size, hidden_size, batch_size, flag_gpu=True):
+        """
+        Seq2Seq + Attentionのインスタンス化
+        :param vocab_size: 語彙数のサイズ
+        :param embed_size: 単語ベクトルのサイズ
+        :param hidden_size: 隠れ層のサイズ
+        :param batch_size: ミニバッチのサイズ
+        :param flag_gpu: GPUを使うかどうか
+        """
+        super(Att_Seq2Seq, self).__init__(
+            # 順向きのEncoder
+            f_encoder = LSTM_Encoder(vocab_size, embed_size, hidden_size),
+            # 逆向きのEncoder
+            b_encoder = LSTM_Encoder(vocab_size, embed_size, hidden_size),
+            # Attention Model
+            attention = Attention(hidden_size, flag_gpu),
+            # Decoder
+            decoder = Att_LSTM_Decoder(vocab_size, embed_size, hidden_size)
+        )
+        self.vocab_size = vocab_size
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.batch_size = batch_size
+
+        # GPUを使うときはcupy、使わないときはnumpy
+        if flag_gpu:
+            self.ARR = cuda.cupy
+        else:
+            self.ARR = np
+
+        # 順向きのEncoderの中間ベクトル、逆向きのEncoderの中間ベクトルを保存するためのリストを初期化
+        self.fs = []
+        self.bs = []
+
+    def encode(self, words):
+        """
+        Encoderの計算
+        :param words: 入力で使用する単語記録されたリスト
+        :return:
+        """
+        # 内部メモリ、中間ベクトルの初期化
+        c = Variable(self.ARR.zeros((self.batch_size, self.hidden_size), dtype='float32'))
+        h = Variable(self.ARR.zeros((self.batch_size, self.hidden_size), dtype='float32'))
+        # 先ずは順向きのEncoderの計算
+        for w in words:
+            c, h = self.f_encoder(w, c, h)
+            # 計算された中間ベクトルを記録
+            self.fs.append(h)
+
+        # 内部メモリ、中間ベクトルの初期化
+        c = Variable(self.ARR.zeros((self.batch_size, self.hidden_size), dtype='float32'))
+        h = Variable(self.ARR.zeros((self.batch_size, self.hidden_size), dtype='float32'))
+        # 逆向きのEncoderの計算
+        for w in reversed(words):
+            c, h = self.b_encoder(w, c, h)
+            # 計算された中間ベクトルを記録
+            self.bs.insert(0, h)
+
+        # 内部メモリ、中間ベクトルの初期化
+        self.c = Variable(self.ARR.zeros((self.batch_size, self.hidden_size), dtype='float32'))
+        self.h = Variable(self.ARR.zeros((self.batch_size, self.hidden_size), dtype='float32'))
+
+    def decode(self, w):
+        """
+        Decoderの計算
+        :param w: Decoderで入力する単語
+        :return: 予測単語
+        """
+        att_f, att_b = self.attention(self.fs, self.bs, self.h)
+        t, self.c, self.h = self.decoder(w, self.c, self.h, att_f, att_b)
+        return t
+
+    def reset(self):
+        """
+        インスタンス変数を初期化する
+        :return:
+        """
+        # 内部メモリ、中間ベクトルの初期化
+        self.c = Variable(self.ARR.zeros((self.batch_size, self.hidden_size), dtype='float32'))
+        self.h = Variable(self.ARR.zeros((self.batch_size, self.hidden_size), dtype='float32'))
+        # Encoderの中間ベクトルを記録するリストの初期化
+        self.fs = []
+        self.bs = []
+        # 勾配の初期化
+        self.zerograds()
+
+
+# In[ ]:
+
 def forward(enc_words, dec_words, model, ARR):
     """
     順伝播の計算
@@ -296,7 +469,7 @@ def train():
                                  model=model,
                                  ARR=ARR)
             # 学習
-            logger.info("train loss: {0}".format(total_loss.data))
+            logger.info("train loss: {0}".format(total_loss))
             total_loss.backward()
             opt.update()
             opt.zero_grads()
